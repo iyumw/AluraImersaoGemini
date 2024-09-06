a592b5bbfbce113911173212626f64469804b58b
Isis Yume Okamoto <isis.yume2508@gmail.com>
2024-09-06 19:39:30 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # AluraImersaoGemini
+
+### Descrição
+Este repositório contém uma base de conhecimento completa sobre o universo de Ordem Paranormal. Aqui você encontrará informações detalhadas sobre as campanhas, graphic novels e ocultismo da série. A base de dados é alimentada diretamente pela wiki oficial da Ordem Paranormal, permitindo que você encontre rapidamente as informações que procura através da nossa barra de pesquisa.
+
+### Tecnologias Utilizadas
+**HTML**: Estruturação da página
+**CSS**: Estilização visual
+**JavaScript**: Interatividade 
+
+#### Recursos Adicionais
+**Wiki Oficial:** [\[Link para a wiki oficial\]](https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal_Wiki)
---
+++ dados.js
@@ -0,0 +1,37 @@
+let dados = [
+    {
+        titulo : "A Ordem Paranormal",
+        descricao : "A Ordem Paranormal é a primeira temporada da série Ordem Paranormal. É uma campanha de RPG de mesa baseada no sistema de The Esoterrorists (Os Esoterroristas, em português) de Robin D. Laws. Mestrada pelo streamer e youtuber Cellbit, é protagonizada por Alexsander Kothe, Daniel Hartmann, Elizabeth Webber, Thiago Fritz.",
+        link : "https://ordemparanormal.fandom.com/wiki/A_Ordem_Paranormal_(campanha)"
+    },
+    {
+        titulo : "O Segredo na Floresta",
+        descricao : "O Segredo na Floresta é a segunda temporada da série Ordem Paranormal, continuação de A Ordem Paranormal. É uma campanha de RPG de mesa baseada em uma versão adaptada do sistema Chamado de Cthulhu (do inglês, Call of Cthulhu). Mestrada pelo streamer Cellbit, é protagonizada por Cesar Oliveira Cohen, Cristopher Cohen, Elizabeth Webber, Joui Jouki, Thiago Fritz e posteriormente Arthur Cervero.",
+        link : "https://ordemparanormal.fandom.com/wiki/O_Segredo_na_Floresta"
+    },
+    {
+        titulo : "Desconjuração",
+        descricao : "Ordem Paranormal: Desconjuração é a terceira temporada da série Ordem Paranormal, continuação de O Segredo na Floresta. É uma campanha de RPG de mesa baseado em uma versão adaptada pelo mestre do sistema Chamado de Cthulhu (do inglês, Call of Cthulhu). Mestrada pelo streamer e diretor criativo de Enigma do Medo, Cellbit, é protagonizada por Arthur Cervero, Beatrice Portinari, Dante, Elizabeth Webber, Erin Parker, Fernando Carvalho, Joui Jouki, Kaiser e Luciano Carvalho.",
+        link : "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal:_Desconjura%C3%A7%C3%A3o"
+    },
+    {
+        titulo : "Calamidade",
+        descricao : "Ordem Paranormal: Calamidade é a quarta temporada da série Ordem Paranormal, continuação de Ordem Paranormal: Desconjuração. É uma campanha de RPG de mesa baseada no sistema de Ordem Paranormal RPG, criado pelo mestre junto da equipe de Skyfall RPG (Pedro Coimbra e Silvia Sala) e da equipe de Tormenta 20 (Felipe Della Corte e Guilherme Dei Svaldi). Mestrada pelo streamer, diretor criativo e roteirista de Enigma do Medo, Cellbit, é protagonizada por dois lados: do lado da Ordem, sendo Antônio Pontevedra, Arthur Cervero, Carina Leone, Dante, Rubens Naluti e posteriormente Joui Jouki; do lado de Kian, sendo Artemis Deordelin Rodrigues, Boris Lukic, Damir Lukic, Gal e Theodore Bagwell.",
+        link : "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal:_Calamidade"
+    },
+    {
+        titulo : "O Segredo na Ilha",
+        descricao : "O Segredo na Ilha é a primeira campanha spin-off da série Ordem Paranormal. É uma campanha de RPG de mesa baseada no sistema Ordem Paranormal RPG. Mestrada pelo streamer, diretor criativo e roteirista de Enigma do Medo, Cellbit, é protagonizada por Amelie Florence, Bárbara Lima, Milo Castello, Olivier Florence e Wanderley Nascimento de Jesus Maria.",
+        link : "https://ordemparanormal.fandom.com/wiki/O_Segredo_na_Ilha"
+    },
+    {
+        titulo : "Sinais do Outro Lado",
+        descricao : "Sinais do Outro Lado é a segunda campanha spin-off da série Ordem Paranormal. É uma campanha de RPG de mesa baseada no sistema Ordem Paranormal RPG. Mestrada pelo streamer, diretor criativo e roteirista de Enigma do Medo, Cellbit, é protagonizada por Alexandre, Chico Albuquerque, Dara Venturini, Guilherme Santos e Lirio Tellini.",
+        link : "https://ordemparanormal.fandom.com/wiki/Sinais_do_Outro_Lado"
+    },
+    {
+        titulo : "Quarentena",
+        descricao : "Ordem Paranormal: Quarentena é uma sessão especial de dois episódios da série Ordem Paranormal. É uma campanha de RPG two-shot baseada em Ordem Paranormal RPG, sistema oficial da marca, utilizando o suplemento Sobrevivendo ao Horror pela primeira vez. Mestrada pelo streamer, diretor criativo e roteirista de Enigma do Medo, Cellbit, é protagonizada por cinco criadores de conteúdo globais, interpretando Benito Camelo, Diego Thalles, Jeffrey Bacon, Lucie Pocharde e Luis Miguel Kennedy. Essa é a primeira campanha da série feita com criadores de conteúdo internacionais, tendo como língua principal o Inglês.",
+        link : "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal:_Quarentena"
+    }
+];
