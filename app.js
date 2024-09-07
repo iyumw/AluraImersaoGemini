948649393f3916c30c7b3eb728bac8f18f64f08c
Isis Yume Okamoto <isis.yume2508@gmail.com>
2024-09-07 16:53:20 -0300

    update aula 5
    funcionalidade de pesquisa

--- app.js
+++ app.js
@@ -1,24 +1,51 @@
-function pesquisar(){
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+    // Verifica se a seção foi encontrada no DOM (para depuração)
+    
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;
 
+    if (!campoPesquisa) {
+        section.innerHTML = "<p class='sem-resultados'>Nenhum resultado encontrado. Por favor digite sua busca na barra de pesquisa :P</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase();
+    
+    // Inicializa uma string vazia para armazenar os resultados da pesquisa
+    let resultados = "";
+
+    let titulo = "";
+    let descricao = "";
+    let tipo = "";
+
+    // Itera sobre cada dado na lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tipo = dado.tipo.toLowerCase();
+
+        console.log(dado.titulo.includes(campoPesquisa));
+        if (titulo.includes(campoPesquisa) || tipo.includes(campoPesquisa)){
+            //Cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="${dado.link}" target="_blank"> ${dado.titulo} </a>
+                </h2>
+                <p class="descricao-meta">
+                    ${dado.descricao}
+                </p>
+                <a href="${dado.link}" target="_blank">Saiba mais</a>
+            </div>
+        `;
+        }
+    }
+
+if(!resultados){
+        resultados = "<p class='sem-resultados'>Nenhum resultado encontrado. >:D</p>"
+    }
+
+// Atribui o HTML gerado à seção de resultados
+    section.innerHTML = resultados;  
 }
-
-let section = document.getElementById("resultados-pesquisa");
-console.log(section);
-
-let resultados = "";
-
-for (let dado of dados) {
-    resultados += `
-        <div class="item-resultado">
-            <h2>
-                <a href="${dado.link}" target="_blank"> ${dado.titulo} </a>
-            </h2>
-            <p class="descricao-meta">
-                ${dado.descricao}
-            </p>
-            <a href="${dado.link}" target="_blank">Saiba mais</a>
-        </div>
-`  
-}
-
-section.innerHTML = resultados;
--- dados.js
+++ dados.js
@@ -2,36 +2,133 @@ let dados = [
     {
         titulo : "A Ordem Paranormal",
         descricao : "A Ordem Paranormal é a primeira temporada da série Ordem Paranormal. É uma campanha de RPG de mesa baseada no sistema de The Esoterrorists (Os Esoterroristas, em português) de Robin D. Laws. Mestrada pelo streamer e youtuber Cellbit, é protagonizada por Alexsander Kothe, Daniel Hartmann, Elizabeth Webber, Thiago Fritz.",
-        link : "https://ordemparanormal.fandom.com/wiki/A_Ordem_Paranormal_(campanha)"
+        link : "https://ordemparanormal.fandom.com/wiki/A_Ordem_Paranormal_(campanha)",
+        tipo : "Temporadas"
     },
     {
         titulo : "O Segredo na Floresta",
         descricao : "O Segredo na Floresta é a segunda temporada da série Ordem Paranormal, continuação de A Ordem Paranormal. É uma campanha de RPG de mesa baseada em uma versão adaptada do sistema Chamado de Cthulhu (do inglês, Call of Cthulhu). Mestrada pelo streamer Cellbit, é protagonizada por Cesar Oliveira Cohen, Cristopher Cohen, Elizabeth Webber, Joui Jouki, Thiago Fritz e posteriormente Arthur Cervero.",
-        link : "https://ordemparanormal.fandom.com/wiki/O_Segredo_na_Floresta"
+        link : "https://ordemparanormal.fandom.com/wiki/O_Segredo_na_Floresta",
+        tipo : "Temporadas"
     },
     {
         titulo : "Desconjuração",
         descricao : "Ordem Paranormal: Desconjuração é a terceira temporada da série Ordem Paranormal, continuação de O Segredo na Floresta. É uma campanha de RPG de mesa baseado em uma versão adaptada pelo mestre do sistema Chamado de Cthulhu (do inglês, Call of Cthulhu). Mestrada pelo streamer e diretor criativo de Enigma do Medo, Cellbit, é protagonizada por Arthur Cervero, Beatrice Portinari, Dante, Elizabeth Webber, Erin Parker, Fernando Carvalho, Joui Jouki, Kaiser e Luciano Carvalho.",
-        link : "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal:_Desconjura%C3%A7%C3%A3o"
+        link : "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal:_Desconjura%C3%A7%C3%A3o",
+        tipo : "Temporadas"
     },
     {
         titulo : "Calamidade",
         descricao : "Ordem Paranormal: Calamidade é a quarta temporada da série Ordem Paranormal, continuação de Ordem Paranormal: Desconjuração. É uma campanha de RPG de mesa baseada no sistema de Ordem Paranormal RPG, criado pelo mestre junto da equipe de Skyfall RPG (Pedro Coimbra e Silvia Sala) e da equipe de Tormenta 20 (Felipe Della Corte e Guilherme Dei Svaldi). Mestrada pelo streamer, diretor criativo e roteirista de Enigma do Medo, Cellbit, é protagonizada por dois lados: do lado da Ordem, sendo Antônio Pontevedra, Arthur Cervero, Carina Leone, Dante, Rubens Naluti e posteriormente Joui Jouki; do lado de Kian, sendo Artemis Deordelin Rodrigues, Boris Lukic, Damir Lukic, Gal e Theodore Bagwell.",
-        link : "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal:_Calamidade"
+        link : "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal:_Calamidade",
+        tipo : "Temporadas"
     },
     {
         titulo : "O Segredo na Ilha",
         descricao : "O Segredo na Ilha é a primeira campanha spin-off da série Ordem Paranormal. É uma campanha de RPG de mesa baseada no sistema Ordem Paranormal RPG. Mestrada pelo streamer, diretor criativo e roteirista de Enigma do Medo, Cellbit, é protagonizada por Amelie Florence, Bárbara Lima, Milo Castello, Olivier Florence e Wanderley Nascimento de Jesus Maria.",
-        link : "https://ordemparanormal.fandom.com/wiki/O_Segredo_na_Ilha"
+        link : "https://ordemparanormal.fandom.com/wiki/O_Segredo_na_Ilha",
+        tipo : "Temporadas, Spin-off, Spin-offs"
     },
     {
         titulo : "Sinais do Outro Lado",
         descricao : "Sinais do Outro Lado é a segunda campanha spin-off da série Ordem Paranormal. É uma campanha de RPG de mesa baseada no sistema Ordem Paranormal RPG. Mestrada pelo streamer, diretor criativo e roteirista de Enigma do Medo, Cellbit, é protagonizada por Alexandre, Chico Albuquerque, Dara Venturini, Guilherme Santos e Lirio Tellini.",
-        link : "https://ordemparanormal.fandom.com/wiki/Sinais_do_Outro_Lado"
+        link : "https://ordemparanormal.fandom.com/wiki/Sinais_do_Outro_Lado",
+        tipo : "Temporadas, Spin-off, Spin-offs"
     },
     {
         titulo : "Quarentena",
         descricao : "Ordem Paranormal: Quarentena é uma sessão especial de dois episódios da série Ordem Paranormal. É uma campanha de RPG two-shot baseada em Ordem Paranormal RPG, sistema oficial da marca, utilizando o suplemento Sobrevivendo ao Horror pela primeira vez. Mestrada pelo streamer, diretor criativo e roteirista de Enigma do Medo, Cellbit, é protagonizada por cinco criadores de conteúdo globais, interpretando Benito Camelo, Diego Thalles, Jeffrey Bacon, Lucie Pocharde e Luis Miguel Kennedy. Essa é a primeira campanha da série feita com criadores de conteúdo internacionais, tendo como língua principal o Inglês.",
-        link : "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal:_Quarentena"
+        link : "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal:_Quarentena",
+        tipo : "Temporadas, Spin-off, Spin-offs"
+    },
+    {
+        titulo : "Ordem Paranormal: Iniciação",
+        descricao : "Ordem Paranormal: Iniciação é a primeira graphic novel da série Ordem Paranormal. É uma adaptação da campanha de RPG de mesa, A Ordem Paranormal, baseada no sistema The Esoterrorists, de Robin D. Laws. É protagonizada por Alexsander Kothe, Daniel Hartmann, Elizabeth Webber e Thiago Fritz.",
+        link : "https://ordemparanormal.fandom.com/wiki/Ordem_Paranormal:_Inicia%C3%A7%C3%A3o",
+        tipo : "Graphic Novels, HQ, HQs"
+    },
+    {
+        titulo : "O Segredo na Floresta - Parte 1",
+        descricao : "O Segredo na Floresta - Parte 1 é a segunda graphic novel da série Ordem Paranormal e a primeira parte (episódios 1 ao 8) do arco da segunda temporada. É uma adaptação da campanha de RPG de mesa, O Segredo na Floresta, baseada em uma versão adaptada do sistema Chamado de Cthulhu. É protagonizada por Arthur Cervero, Cesar Oliveira, Cristopher Cohen, Elizabeth Webber, Joui Jouki e Thiago Fritz.",
+        link : "https://ordemparanormal.fandom.com/wiki/O_Segredo_na_Floresta_-_Parte_1_(graphic_novel)",
+        tipo : "Graphic Novels, HQ, HQs"
+    },
+    {
+        titulo : "O Segredo na Floresta - Parte 2",
+        descricao : "O Segredo na Floresta - Parte 2 é a terceira graphic novel da série Ordem Paranormal, sendo a continuidade da Parte 1 referente ao arco da segunda temporada. É uma adaptação da campanha de RPG de mesa, O Segredo na Floresta, baseada em uma versão adaptada do sistema Chamado de Cthulhu. É protagonizada por Arthur Cervero, Cesar Oliveira Cohen, Elizabeth Webber, Joui Jouki e Thiago Fritz.",
+        link : "https://ordemparanormal.fandom.com/wiki/O_Segredo_na_Floresta_-_Parte_2_(graphic_novel)",
+        tipo : "Graphic Novels, HQ, HQs"
+    },
+    {
+        titulo : "Elementos do Outro Lado",
+        descricao : "Os Elementos do Outro Lado são uma das principais fundações do Ocultismo. Esses que podem ser divididos em cinco principais: Sangue, Morte, Conhecimento, Energia e Medo. Cada um desses elementos possuem propriedades únicas e são responsáveis por definir as características das materializações do Outro Lado na Realidade.",
+        link : "https://ordemparanormal.fandom.com/wiki/Elementos_do_Outro_Lado",
+        tipo : "Ocultismo"
+    },
+    {
+        titulo : "Sangue",
+        descricao : "O Sangue é a entidade do sentimento. Ele busca a intensidade: dor, obsessão, paixão, amor, fome, ódio - tudo que envolve sentir uma emoção extrema agrada a entidade de Sangue.",
+        link : "https://ordemparanormal.fandom.com/wiki/Elementos_do_Outro_Lado#Elemento_de_Sangue",
+        tipo : "Ocultismo, Elementos"
+    },
+    {
+        titulo : "Morte",
+        descricao : "A Morte é a entidade do tempo. Ela busca os momentos vivenciados, distorcendo a percepção egóica da existência de cada indivíduo para seu próprio agrado. A distorção temporal da Morte arruína a percepção carnal do Sangue.",
+        link : "https://ordemparanormal.fandom.com/wiki/Elementos_do_Outro_Lado#Elemento_de_Morte",
+        tipo : "Ocultismo, Elementos"
+    },
+    {
+        titulo : "Conhecimento",
+        descricao : "O Conhecimento é a entidade da consciência. Descobrir, aprender, conhecer, decifrar. Ter a própria percepção do Outro Lado e suas entidades agrada o elemento de Conhecimento. A razão e lógica do Conhecimento reintegram e suprimem o caos da Energia.",
+        link : "https://ordemparanormal.fandom.com/wiki/Elementos_do_Outro_Lado#Elemento_de_Conhecimento",
+        tipo : "Ocultismo, Elementos"
+    },
+    {
+        titulo : "Energia",
+        descricao : "A Energia é a entidade do caos. Tudo que não pode ser explicado, o intangível, a anarquia. A constante mudança, o calor e o frio, a luz e as trevas. Tudo que envolve a imprevisibilidade e a transformação agrada a entidade de Energia. A transformação da Energia sobrecarrega os efeitos da Morte.",
+        link : "https://ordemparanormal.fandom.com/wiki/Elementos_do_Outro_Lado#Elemento_de_Energia",
+        tipo : "Ocultismo, Elementos"
+    },
+    {
+        titulo : "Medo",
+        descricao : "O Medo é a entidade do desconhecido e do infinito. Presente desde os primórdios da humanidade, modifica a natureza do universo. A sua existência diferenciada é um mistério.",
+        link : "https://ordemparanormal.fandom.com/wiki/Elementos_do_Outro_Lado#Elemento_de_Medo",
+        tipo : "Ocultismo, Elementos"
+    },
+    {
+        titulo : "Relíquias da Calamidade",
+        descricao : "As Relíquias da Calamidade são objetos paranormais extremamente poderosos que só podem ser usadas por Marcados. Portar uma Relíquia é o equivalente a ter uma completa Exposição Paranormal, possibilitando também moldar o Paranormal e seus 'Pilares'. A maior concentração de informações sobre as Relíquias é o Diário de Deus, um grimório que esteve em posse de Arnaldo Fritz enquanto ele dedicava muito tempo de sua vida pesquisando sobre as Relíquias, anexando ali suas descobertas.",
+        link : "https://ordemparanormal.fandom.com/wiki/Rel%C3%ADquias_da_Calamidade",
+        tipo : "Ocultismo"
+    },
+    {
+        titulo : "Símbolos Ocultistas",
+        descricao : "Símbolos Ocultistas são um dos principais componentes do Ocultismo. Existem símbolos com propósitos diferentes, alguns são utilizados para realizar rituais enquanto outros são utilizados para representar Entidades, como no caso das Marcas.",
+        link : "https://ordemparanormal.fandom.com/wiki/S%C3%ADmbolos_Ocultistas",
+        tipo : "Ocultismo"
+    },
+    {
+        titulo : "Rituais",
+        descricao : "Rituais são habilidades Ocultistas no universo de Ordem Paranormal, vistos pela primeira vez em A Ordem Paranormal e sendo mais aprofundados em Ordem Paranormal: Desconjuração. Desde então, os rituais têm presença muito forte na série, sendo utilizados tanto por ocultistas quanto até mesmo pelos protagonistas. Os rituais apresentam vários usos diferentes, mas no geral são utilizados para trazer uma vantagem ao usuário, seja em um combate ou até mesmo no dia a dia. Todos os rituais têm características, como a relação com Elementos do Outro Lado e ingredientes que são necessários para realizar certos rituais, como cinzas humanas e animais, objetos tecnológicos, entre outros.",
+        link : "https://ordemparanormal.fandom.com/wiki/Rituais",
+        tipo : "Ocultismo"
+    },
+    {
+        titulo : "Classes",
+        descricao : "As Classes são uma série de diferentes estilos de jogo criados e introduzidos na série Ordem Paranormal. Elas foram introduzidas em Ordem Paranormal: Desconjuração com o nome Arquétipos como rotas de construção do estilo e papel de seu personagem tanto em investigação quanto em batalha, concedendo habilidades e bônus de perícias como recompensas. Em Ordem Paranormal: Calamidade, o nome definitivo passou a ser Classes, tendo a adição de um conjunto de subclasses conhecidas como Trilhas, que de maneira criativa, mudam o modo de como o jogador pode utilizar suas habilidades para sobreviver no universo.",
+        link : "https://ordemparanormal.fandom.com/wiki/Classes",
+        tipo : "Ocultismo"
+    },
+    {
+        titulo : "Marcas",
+        descricao : "As Marcas são misteriosas entidades paranormais que escolhem indivíduos para se conectar através do Medo. Esses indivíduos, os Marcados, são os seres mais perigosos e poderosos da Realidade, capazes de se conectar com o Medo e invocá-lo diretamente por meio de suas Marcas.",
+        link : "https://ordemparanormal.fandom.com/wiki/Marcas",
+        tipo : "Ocultismo"
+    },
+    {
+        titulo : "Enigma do Medo",
+        descricao : "Enigma do Medo é um futuro jogo de exploração, enigmas e sobrevivência com uma narrativa profunda e envolvente no universo de RPG criado por Rafael Lange (Cellbit) que está sendo desenvolvido em parceria com a Dumativa e foi financiado por um projeto no Catarse. Ambientado no universo de Ordem Paranormal, o jogo contará com personagens e criaturas já conhecidos além de novos que serão trazidos pela obra. Foi anunciado em 17 de outubro de 2020 e tem lançamento previsto para 2024.",
+        link : "https://www.nuuvem.com/lp/pt/enigma-do-medo/",
+        tipo : "Jogo"
     }
 ];
